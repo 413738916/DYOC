fcaa33604215cbcc2274344be91127f439206494
123 <a123@123deMac-mini.local>
2017-11-02 15:35:12 +0800

    初始化

---
+++ DYOC/DYOC/Classes/Main/Controller/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  DYOC
+//
+//  Created by 123 on 2017/11/2.
+//  Copyright © 2017年 ct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UITabBarController
+
+@end
---
+++ DYOC/DYOC/Classes/Main/Controller/MainViewController.m
@@ -0,0 +1,53 @@
+//
+//  MainViewController.m
+//  DYOC
+//
+//  Created by 123 on 2017/11/2.
+//  Copyright © 2017年 ct. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UITabBar.appearance.tintColor = [UIColor orangeColor];
+    
+    [self addChildVC:@"Home"];
+    [self addChildVC:@"Live"];
+    [self addChildVC:@"Follow"];
+    [self addChildVC:@"Me"];
+    
+}
+
+- (void)addChildVC:(NSString *)storyName {
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:storyName bundle:nil];
+    UIViewController *childVC = [storyboard instantiateInitialViewController];
+    [self addChildViewController:childVC];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
